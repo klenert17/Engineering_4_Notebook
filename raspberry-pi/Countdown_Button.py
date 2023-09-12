6f83ccaa1054dedaecc032caa8ab5285534947f7
klenert17 <klenert17@charlottesvilleschools.org>
2023-09-12 13:23:09 -0400

    Finished Launch 3 Code

--- raspberry-pi/Countdown_Button.py
+++ raspberry-pi/Countdown_Button.py
@@ -2,18 +2,35 @@
 import time
 import board
 import digitalio
+from digitalio import DigitalInOut, Direction, Pull
 
 led1 = digitalio.DigitalInOut(board.GP18)
 led1.direction = digitalio.Direction.OUTPUT
 led2 = digitalio.DigitalInOut(board.GP13)
 led2.direction = digitalio.Direction.OUTPUT # red and green led output location
 
-for x in range(10, 0, -1): # in the range from 10 to 0 going down by 1
-    print(x) # print the variable
-    led1.value = True
-    time.sleep(0.5)
-    led1.value = False
-    time.sleep(0.5) # blink red led
-print("Launch")
-led2.value = True
-time.sleep(5.0) # green led on for 5 secs
+btn = DigitalInOut(board.GP15)
+btn.direction = Direction.INPUT
+btn.pull = Pull.UP # set pin number and input as pull up
+
+prev_state = btn.value # new variable
+
+while True:
+    cur_state = btn.value # new variable
+    if cur_state != prev_state: # if the state of the button changes
+        if not cur_state: # if the button is pressed
+            print("Button pushed")
+            for x in range(10, 0, -1): # in the range from 10 to 0 going down by 1
+                print(x) # print the variable
+                led1.value = True
+                time.sleep(0.5)
+                led1.value = False
+                time.sleep(0.5) # blink red led
+            led2.value = True 
+            print("Launch")
+            time.sleep(3.0)
+            led2.value = False # turn on green light and print liftoff then turn the light off after 3 seconds
+        else:
+            print("Button is up")
+
+    prev_state = cur_state # reset button value to unpressed
