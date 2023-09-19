4b2304beb8bff007907e5c9d58fc8a7cc469c1b4
klenert17 <klenert17@charlottesvilleschools.org>
2023-09-19 14:15:27 -0400

    work in progress code

---
+++ raspberry-pi/CrashAvoidence2.py
@@ -0,0 +1,31 @@
+#type: ignore
+
+import board
+import digitalio
+import time
+import adafruit_mpu6050
+import busio
+
+
+sda_pin = board.GP14
+scl_pin = board.GP15
+i2c = busio.I2C(board.GP15, board.GP14)
+mpu = adafruit_mpu6050.MPU6050(i2c)
+mpu.acceleration
+led1 = digitalio.DigitalInOut(board.GP0)
+led1.direction = digitalio.Direction.OUTPUT
+
+
+while True: #loops command
+    
+    print(f"x angular acceleration: {mpu.acceleration[0]}") #print x value
+    print(f"y angular acceleration: {mpu.acceleration[1]}") #print y value
+    print(f"z angular acceleration: {mpu.acceleration[2]}") #print z value
+    print("") #prints a gap
+    time.sleep(0.5) #pauses for half a second before printing again
+    led1.value = True
+
+    if mpu.acceleration[0] < 9 and mpu.acceleration[0] > -9:
+        print("no light")
+        led1.value = False
+
