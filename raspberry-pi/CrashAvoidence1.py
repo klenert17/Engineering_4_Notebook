37d90875a04516288be59761f7adc75686487f8b
klenert17 <klenert17@charlottesvilleschools.org>
2023-09-19 13:17:54 -0400

    FInished Code 1

--- raspberry-pi/CrashAvoidence1.py
+++ raspberry-pi/CrashAvoidence1.py
@@ -15,9 +15,9 @@ mpu.acceleration
 
 while True: #loops command
     
-    print(f"x angular velocity: {mpu.gyro[0]}") #print x value
-    print(f"y angular velocity: {mpu.gyro[1]}") #print y value
-    print(f"z angular velocity: {mpu.gyro[2]}") #print z value
+    print(f"x angular acceleration: {mpu.acceleration[0]}") #print x value
+    print(f"y angular acceleration: {mpu.acceleration[1]}") #print y value
+    print(f"z angular acceleration: {mpu.acceleration[2]}") #print z value
     print("") #prints a gap
     time.sleep(0.5) #pauses for half a second before printing again
     
