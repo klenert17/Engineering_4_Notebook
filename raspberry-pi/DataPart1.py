33eeb839a8d61bf6399563d3d8f378e757ad2562
klenert17 <klenert17@charlottesvilleschools.org>
2023-11-09 13:54:34 -0500

    Finished Code

---
+++ raspberry-pi/DataPart1.py
@@ -0,0 +1,30 @@
+#type: ignore
+
+import board
+import digitalio
+import time
+import adafruit_mpu6050
+import busio
+
+
+sda_pin = board.GP14
+scl_pin = board.GP15
+i2c = busio.I2C(board.GP15, board.GP14)
+mpu = adafruit_mpu6050.MPU6050(i2c)
+mpu.acceleration
+led1 = digitalio.DigitalInOut(board.GP1)
+led1.direction = digitalio.Direction.OUTPUT
+
+with open("/data.csv", "a") as datalog:
+    while True: #loops command
+
+        time.sleep(0.5) #pauses for half a second before printing again
+        datalog.write(f"{time.monotonic()},{mpu.acceleration[0]},{mpu.acceleration[1]},{mpu.acceleration[2]},{led1.value},\n") #print x value
+        datalog.flush()
+
+        led1.value = False #makes LED set to off
+
+        if mpu.acceleration[0] > 9 or mpu.acceleration[0] < -9: #command when board is turned 90 degrees
+            print("light")
+            led1.value = True #turns LED on if it is tilted 90 degrees
+
